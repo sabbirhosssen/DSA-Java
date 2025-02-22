c16fac2f40d3ce1061ae6743a2cd0eb6528750c5
@sabbirhosssen <sabbirhossen58sh58@gmail.com>
2025-02-22 21:07:26 +0600

    Arraylist updates

--- src/Collection/ArrayList/ArrayList.java
+++ src/Collection/ArrayList/ArrayList.java
@@ -3,5 +3,9 @@ package Collection.ArrayList;
 public class ArrayList {
     public static void main(String[] args) {
         System.out.println("ArrayList");
+        System.out.println("ArrayList");
+        System.out.println("ArrayList");
+        System.out.println("ArrayList");
+        System.out.println("ArrayList");
     }
 }
